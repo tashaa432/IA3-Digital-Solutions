eb9f3d40163fc422513ef3edd94b42c7f04be56e
Tasha <101168@salc.qld.edu.au>
2024-04-15 11:35:25 +1000

    added more styling

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles/main.css">
     <title>Quiz Display</title>
 </head>
@@ -20,8 +23,7 @@
     if ($data !== null) {
         // Check response_code and display appropriate message
         if ($data['response_code'] === 0) {
-            echo "<p>Data loaded correctly.</p>";
-            echo "<br>";
+            
 
             // Display questions and answers
             foreach ($data['results'] as $result) {
@@ -34,10 +36,26 @@
                     echo "<li class='incorrect'>" . htmlspecialchars_decode($incorrect_answer) . "</li>";
                 }
                 echo "</ul>";
+                echo "<br>";
             }
         } else {
-            echo "An error occurred:";
+            // Display the correct error message based on the error code
+            $error_messages = [
+                1 => "No Results: The API doesn't have enough questions for your query.",
+                2 => "Invalid Parameter: Contains an invalid parameter. Arguments passed in aren't valid.",
+                3 => "Token Not Found: Session Token does not exist.",
+                4 => "Token Empty: Session Token has returned all possible questions for the specified query. Resetting the Token is necessary.",
+                5 => "Rate Limit: Too many requests have occurred."
+            ];
+    
+            $error_code = $data['response_code'];
+            if (isset($error_messages[$error_code])) {
+                echo "An error occurred: " . $error_messages[$error_code] . " (Error Code: $error_code)";
+            } else {
+                echo "An unknown error occurred. (Error Code: $error_code)";
+            }
         }
+    
     }
     ?>
 
